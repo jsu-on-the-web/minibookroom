4e8e9536c07d7411301cb1914122070a22bc7cfe
JS <janet@suryadi.com>
2026-01-18 14:32:24 +1100

    Fixing styles, adding Header element, and adding FontAwesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/App.scss'
 import Card from './ui-elements/Card/Card'
 import { fetchBook } from './SearchFunctions';
 import CoverPlaceholder from './assets/cover-placeholder.svg';
+import { Header } from './ui-elements/Header/Header';
 
 // Interfaces for prototyping
 interface Book {
@@ -24,23 +25,25 @@ const App = () => {
 
   return (
     <>
-      <section className="flex flex-col items-center p-4 app-container">
-        <h1>Hello world!</h1>
-        <section className='flex flex-row gap-2 items-between searchbar-container'>
-          <input type="text" placeholder="Search for books..." id='searchbar-books' className="px-4 py-2 rounded-lg searchbar-input border-1" />
-          <button className="searchbutton" onClick={() => { setCurrentSearch((document.getElementById('searchbar-books') as HTMLInputElement)?.value || '') }}>Search</button>
+      <section className="min-h-screen app bg-stone-200">
+        <Header title="Mini Bookroom" />
+        <section className="flex flex-col items-center p-4 app-container">
+          <section className='flex flex-row gap-2 items-between searchbar-container'>
+            <input type="text" placeholder="Search for books..." id='searchbar-books' className="px-4 py-2 rounded-lg searchbar-input border-1" />
+            <button className="searchbutton" onClick={() => { setCurrentSearch((document.getElementById('searchbar-books') as HTMLInputElement)?.value || '') }}>Search</button>
+          </section>
+          <div className="flex flex-wrap justify-center gap-4 mt-4 card-container">
+            {currentBooks && currentBooks.map((book, index) => (
+              <Card
+                key={index}
+                title={book.title || "TITLE MISSING"}
+                author={book.author_name?.[0] || "AUTHOR MISSING"}
+                cover={book.imageUrl || CoverPlaceholder}
+                onClick={() => { console.log(`Clicked on ${book.title}`) }}
+              />
+            ))};
+          </div>
         </section>
-        <div className="flex flex-wrap justify-center gap-4 mt-4 card-container">
-          {currentBooks && currentBooks.map((book, index) => (
-            <Card
-              key={index}
-              title={book.title || "TITLE MISSING"}
-              author={book.author_name?.[0] || "AUTHOR MISSING"}
-              cover={book.imageUrl || CoverPlaceholder}
-              onClick={() => { console.log(`Clicked on ${book.title}`) }}
-            />
-          ))};
-        </div>
       </section>
     </>
   )
---
+++ src/ui-elements/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
+interface HeaderProps {
+    title: string,
+}
+
+export const Header: React.FC<HeaderProps> = ({ title }) => {
+    return (
+        <header className="w-full mb-4 shadow-md bg-stone-300 header">
+            <div className="w-full px-4 py-4 mx-auto header-container">
+                <nav className="flex items-center justify-between header-nav">
+                    <FontAwesomeIcon icon={faBars} id="header-menu" className="text-2xl header-icon" />
+                    <h1 className="header-title text-black-800">{title}</h1>
+                    <FontAwesomeIcon icon={faUserCircle} id="header-user" className="text-2xl header-icon" />
+                </nav>
+            </div>
+        </header>
+    );
+}
